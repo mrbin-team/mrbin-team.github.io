9d71848ca797dee30a2ba1a97c240a5f72a3150f
Glenn Y. Rolland <glenux@glenux.net>
2017-11-30 23:09:00 +0100

    Fix contact/subscribe

--- js/subscribe_me.js
+++ js/subscribe_me.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    $("#contact_form input, #contact_form textarea").jqBootstrapValidation({
+    $("#subscribe_form input, #subscribe_form textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
             // additional error messages or events
@@ -8,9 +8,7 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
-            var name = $("input#contact_name").val();
-            var email = $("input#contact_email").val();
-            var message = $("textarea#contact_message").val();
+            var email = $("input#subscribe_email").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
@@ -20,34 +18,32 @@ $(function() {
                 url: "https://formspree.io/bonjour@mrbin.fr",
                 type: "POST",
                 data: {
-                    name: name,
                     email: email,
-                    message: message
                 },
                 dataType: "json",
                 cache: false,
                 success: function() {
                     // Success message
-                    $('#contact_success').html("<div class='alert alert-success'>");
-                    $('#contact_success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#subscribe_success').html("<div class='alert alert-success'>");
+                    $('#subscribe_success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#contact_success > .alert-success')
-                        .append("<strong>Votre message a bien été envoyé. </strong>");
-                    $('#contact_success > .alert-success')
+                    $('#subscribe_success > .alert-success')
+                        .append("<strong>Votre inscription a bien été demandée. </strong>");
+                    $('#subscribe_success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#subscribe_form').trigger("reset");
                 },
                 error: function() {
                     // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    $('#subscribe_success').html("<div class='alert alert-danger'>");
+                    $('#subscribe_success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
+                    $('#subscribe_success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#subscribe_success > .alert-danger').append('</div>');
                     //clear all fields
-                    $('#contactForm').trigger("reset");
+                    $('#subscribe_form').trigger("reset");
                 },
             })
         },
